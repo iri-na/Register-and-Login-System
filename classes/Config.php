40008d7399c8ea0c5a9d3dfb29172caf62d0277d
iri-na <imcarcam@uwaterloo.ca>
2020-08-18 13:54:44 -0400

    Finish Config

--- classes/Config.php
+++ classes/Config.php
@@ -6,7 +6,18 @@ class Config {
             $config = $GLOBALS['config'];
             $path = explode('/', $path);
 
-            print_r($path);
+            foreach($path as $bit) { // $path is an array
+                if(isset($config[$bit])) {
+                    $config = $config[$bit];
+                }
+                else {
+                    return false;
+                }
+            }
+            return $config; //returns value of config in this case '127.0.0.1'
+        }
+        else {
+            return false;
         }
     }
 }
