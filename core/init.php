498d95f077d90467c5e69aa6cd0250da2ee6e81d
Irina <irina@Irinas-MacBook-Air.local>
2020-08-18 04:00:45 -0400

    Initial Commit Set Up Classes And Init

---
+++ core/init.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+$GLOBALS['config'] = array(
+    'mysql' => array(
+        'host' => '127.0.0.1',
+        'username' => 'root',
+        'password' => '',
+        'db' => 'LRS'
+    ),
+    'remember' => array(
+        'cookie_name' => 'hash',
+        'cookie_expiry' => 604800 //seconds
+    ),
+    'session' => array(
+        'session_name' => 'user'
+    )
+);
+
+spl_autoload_register(function($class) {
+    require_once 'classes/' . $class . '.php';
+});
+
+require_once 'functions/sanitize.php';
