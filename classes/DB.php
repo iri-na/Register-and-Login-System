630c41a65d8b54a0a633a05feb29e3cf3ca4befe
iri-na <imcarcam@uwaterloo.ca>
2020-08-21 20:49:22 -0400

    Create and Update Methods

--- classes/DB.php
+++ classes/DB.php
@@ -11,7 +11,7 @@ class DB {
         try {
             $this->_pdo = new PDO('mysql:host=' . Config::get('mysql/host') . ';dbname=' . Config::get('mysql/db'),
                 Config::get('mysql/username'), Config::get('mysql/password'));
-        echo 'Connected';
+//        echo 'Connected';
         } catch (PDOException $e) {
             die($e->getMessage());
         }
@@ -41,11 +41,102 @@ class DB {
                 $this->_error = true;
             }
         }
+//        print_r($this->_results);
         return $this;
+    }
+
+    public function action($action, $table, $where = array()) {
+        if(count($where) === 3) {
+            $operators = array('=', '>', '<', '>=', '<=');
+            $field = $where[0];
+            $operator = $where[1];
+            $value = $where[2];
+
+            if(in_array($operator, $operators)) {
+                $sql = "{$action} FROM {$table} WHERE {$field} {$operator} ?";
+                if(!($this->query($sql, array($value)))->error()) {
+                    return $this;
+                }
+            }
+        }
+        return false;
+    }
+
+    public function get($table, $where) {
+        return $this->action('SELECT *', $table, $where);
+    }
+
+    public function delete($table, $where) {
+        return $this->action('DELETE', $table, $where);
+    }
+
+    public function insert($table, $fields = array()) {
+        // all fields of table must be filled in
+        if(count($fields)) {
+            $keys = array_keys($fields);
+            $values = '';
+            $x = 1;
+
+            foreach($fields as $field) {
+                $values .= '?';
+                if($x < count($fields)) {
+                    $values .= ', ';
+                }
+                $x++;
+            }
+
+            $sql = "INSERT INTO {$table} (`" . implode('`, `', $keys) . "`) VALUES ({$values})";
+//            echo $sql;
+
+            if (!($this->query($sql, $fields))->error()) {
+                return true;
+            }
+            else {
+                echo 'there is an error';
+            }
+        }
+        return false;
+    }
+
+    public function update($table, $id, $fields) {
+        $set = '';
+        $x = 1;
+
+        foreach($fields as $name => $value) {
+            $set .= "{$name} = ?";
+            if($x < count($fields)) {
+                $set .= ', ';
+            }
+            $x++;
+        }
+
+        $sql = "UPDATE {$table} SET {$set} WHERE id = {$id}";
+
+        if(!$this->query($sql, $fields)->error()) {
+            return true;
+        }
+        else {
+            print_r($this->query($sql, $fields));
+            echo 'there is an error';
+            return false;
+        }
+    }
+
+    public function results() {
+        return $this->_results;
+    }
+
+    // get only first element in results array
+    public function first() {
+        return $this->results()[0];
     }
 
     public function error() {
         return $this->_error;
     }
 
+    public function count() {
+        return $this->_count;
+    }
 }
+
--- index.php
+++ index.php
@@ -1,10 +1,28 @@
 <?php
 require_once 'core/init.php';
 
-$user = DB::getInstance()->query("SELECT username FROM users WHERE username = ?", array('irina'));
+//$user = DB::getInstance()->get('users', array('username', '=', 'irina'));
+//$user = DB::getInstance()->query("SELECT * FROM users");
 
-if ($user->error()) {
-    echo 'No user';
-} else {
-    echo 'OK!';
-}
+//if (!$user->count()) {
+//    echo 'No user';
+//} else {
+//    foreach($user->results() as $user) {
+//        print_r($user);
+////        echo $user->username, '<br>';
+//    }
+//}
+
+//$user = DB::getInstance()->insert('users', array(
+//    'id' => '9',
+//    'username' => 'hong',
+//    'password' => 'password',
+//    'salt' => '',
+//    'name' => '',
+//    'joined' => '2020-08-20 12:21:52',
+//    'group' => '1'
+//));
+
+$user = DB::getInstance()->update('users', '3', array(
+    'password' => 'hi!!'
+));
